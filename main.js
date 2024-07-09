33a3a5b902262d9ad0aff9a6937eeae23e170aff
MinhajUHR <148472360+MinhajUHR@users.noreply.github.com>
2024-07-09 20:35:11 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Computer Engineer", "Game Developer", "Python Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
